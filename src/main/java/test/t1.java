3c6d08f5408a090ffbf8e1b67092388be2884c07
wang <2694425044@qq.com>
2022-08-14 16:26:36 +0800

    test

---
+++ src/main/java/test/t1.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @title:t1
+ * @Author: wangzhen
+ * @Date:22-8-14 下午4:24
+ * @Version:1.0
+ */
+public class t1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
